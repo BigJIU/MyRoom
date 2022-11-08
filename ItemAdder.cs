d63f5679a03d9824f19e518d435317e0c1c23531
bigJIU <1572204178@qq.com>
2022-11-08 23:18:04 +0800

    Add AddSubItem

--- ItemAdder.cs
+++ ItemAdder.cs
@@ -14,8 +14,69 @@ public class SubItemImporter: ScriptableWizard
 {
     public string TargetName;
     public GameObject FatherObject;
-    
+    private SubNode TargetNode;
+    private string lastFileName;
 
+    void OnWizardCreate()
+    {
+        if( Application.isPlaying)
+        {
+            EditorUtility.DisplayDialog("Error", "Suggest Static Item Import", "OK");
+            goto end;
+        }
+
+        string lastPath = EditorPrefs.GetString("SubItem_lastPath", "");
+        lastFileName = EditorPrefs.GetString("SubItem_lastPath", "");
+        Debug.Log(lastPath);
+        Debug.Log(lastFileName);
+        string expFolder = EditorUtility.SaveFolderPanel("Import Item (By Folder)", lastPath, lastFileName);
+        Debug.Log(expFolder);
+        if (expFolder.Length > 0)
+        {
+            var fi = new System.IO.FileInfo(expFolder);
+            EditorPrefs.SetString("SubItem_lastPath", fi.Name);
+            EditorPrefs.SetString("SubItem_lastPath", fi.Directory.FullName);
+            lastFileName = fi.Name;
+            AddItem();
+        }
+        end:;
+    }
+
+    void AddItem()
+    {
+        //SUNCGDataStructure SUNCGData = SUNCGSceneExporter.GenerateDataFromScene();
+        //Update Config Path
+        UpdatePath();
+        AddItemToJSON();
+        AddItemToScene();
+    }
+
+    //TODO: Verify the Existence in the Path
+    void UpdatePath()
+    {
+        
+    }
+    void AddItemToJSON()
+    {
+        SUNCGDataStructure SUNCGData = SceneImporter.GenerateData<SUNCGDataStructure>(EditorPrefs.GetString("origin_JSON"));
+        Node fatherNode = SUNCGData.levels[0].nodes.ToList().Find((node => node.id == FatherObject.name));
+        if (fatherNode.subItems is null) fatherNode.subItems = new SubNode[0];
+        
+        int id = fatherNode.subItems.Length + 1;
+        TargetNode = new SubNode(lastFileName, $"0_{id}_{lastFileName}");
+        
+        SubNode[] tmpArray = fatherNode.subItems;
+        List<SubNode> tmpList = tmpArray.ToList();
+        tmpList.Add(TargetNode);
+        fatherNode.subItems = tmpList.ToArray();
+        
+        EditorPrefs.SetString("origin_JSON",JsonConvert.SerializeObject(SUNCGData));
+    }
+
+    void AddItemToScene()
+    {
+        SUNCGSceneBuilder.NewSubItemBuild(TargetNode,FatherObject.transform);
+    }
 
 
 
@@ -64,6 +125,7 @@ public class ItemImporter: ScriptableWizard
         AddItemToScene();
     }
 
+    //TODO: Verify the Existence in the Path
     void UpdatePath()
     {
         
